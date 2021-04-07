3a1ac2a6deb0b4a1ad45470842c7319dce1cadc4
AlKamva98 <ngantwenistefano@gamail.co.za>
2021-04-07 09:08:21 +0200

    Expert view Customer list page with table of users created

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Amplify from 'aws-amplify';
 import { Questions } from './pages/EndUser/Questions.js';
 import '@aws-amplify/ui/dist/style.css';
 import Register from './components/Register/Register.js';
+import {ExpertViewCustList  } from "./pages/ExpertViewCustList";
 
 
 
@@ -27,6 +28,7 @@ function App(){
         <Route path="/payment"component={Payment}/>
         <Route path="/pricing"component={Pricing}/>
         <Route path="/register"component={Register}/>
+        <Route path="/evcustlist"component={ExpertViewCustList}/>
       </Switch>
       </div>
       </Router>
---
+++ src/pages/columns.js
@@ -0,0 +1,30 @@
+export const COLUMNS =[
+ {
+      Header: 'Id',
+      accessor: 'id'
+ },
+ {
+      Header: 'Email',
+      accessor: 'email'
+ },
+ {
+      Header: 'Answer',
+      accessor: 'answer'
+ },
+ {
+      Header: 'Document',
+      accessor: 'document'
+ },
+ {
+      Header: 'Completed',
+      accessor: 'completed'
+ },
+ {
+      Header: 'Questions ID',
+      accessor: 'questionID'
+ },
+ {
+      Header: 'Report ID',
+      accessor: 'reportID'
+ },
+]
