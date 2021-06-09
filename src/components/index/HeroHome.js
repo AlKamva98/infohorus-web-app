9de72181fc5898e18e49d25724d293295768b821
AlKamva98 <ngantwenistefano@gamail.co.za>
2021-06-09 10:21:21 +0200

    Front end update v2

--- src/components/index/HeroHome.js
+++ src/components/index/HeroHome.js
@@ -1,9 +1,25 @@
-import React, { useState } from 'react';
+import React, {useState,useEffect} from 'react';
 import Modal from '../../utils/Modal';
+import {Link} from 'react-router-dom'
+import Auth from '@aws-amplify/auth';
 
 function HeroHome() {
 
   const [videoModalOpen, setVideoModalOpen] = useState(false);
+  const [signedIn, setSignedIn]= useState(false);
+
+    useEffect(()=>{
+    handleSignedIn();
+    },[])
+
+async function handleSignedIn(){
+ const user = await Auth.currentAuthenticatedUser();
+  if(user !== undefined){
+    setSignedIn(true);
+  }else{
+    setSignedIn(false);
+  }
+}
 
   return (
    <section className="px-2 py-4 bg-white md:px-0">
@@ -17,13 +33,16 @@ function HeroHome() {
                 </h1>
                 <p className="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">Infohorus offers cybersecurity services that combine diverse expertise in the areas of cybersecurity, defensive social engineering, cyber negotiations, intelligence, and other specialized operations to enhance critical infrastructure cyber resilience.</p>
                 <div className="relative flex flex-col sm:flex-row sm:space-x-4">
-                  <a href="#_" className="flex items-center w-full btn px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto">
+                  {signedIn ? (<Link to="/questions" className="flex items-center w-full btn px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto">
+                    Take Assessment
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round"><line x1={5} y1={12} x2={19} y2={12} className /><polyline points="12 5 19 12 12 19" className /></svg>
+                  </Link>):(<Link to="/demo" className="flex items-center w-full btn px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-md sm:mb-0 hover:bg-indigo-700 sm:w-auto">
                     Schedule Demo
                     <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 ml-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round"><line x1={5} y1={12} x2={19} y2={12} className /><polyline points="12 5 19 12 12 19" className /></svg>
-                  </a>
-                  <a href="#_" className="inline-flex items-center justify-center btn px-6 py-3 mb-3 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 hover:text-gray-600">
+                  </Link>)}
+                  <Link to="/about" className="inline-flex items-center justify-center btn px-6 py-3 mb-3 text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 hover:text-gray-600">
                     Learn More
-                  </a>
+                  </Link>
                 </div>
               </div>
             </div>
