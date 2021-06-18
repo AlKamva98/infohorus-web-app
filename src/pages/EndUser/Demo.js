f6d03b290755991a6b16ef88ce717dfe09caee70
AlKamva98 <ngantwenistefano@gamail.co.za>
2021-06-18 11:03:32 +0200

    front end update 1.14.6.21.2

--- src/pages/EndUser/Demo.js
+++ src/pages/EndUser/Demo.js
@@ -1,18 +1,93 @@
 import {Header} from '../../components/index/Header';
-import {Form} from '../../components/Form';
+import Select  from 'react-select';
+import {useForm, Controller } from "react-hook-form";
+import { selectEmpOptions} from '../../testData/selectOptions'
 import Footer from '../../components/index/Footer';
-import React from 'react'
+import React,{useState} from 'react'
 
 export const Demo = () => {
- return (
-  <div>
-   <Header/>
+ const initialFormState = {
+        email:"", lname:"", fname:"", company:"", employees:""
+    };
+    const [formState, updateFormState] = useState(initialFormState);
+    const { errors, control } = useForm();
+   function onChange(e){
+        e.persist()
+        console.log("changing:"+e.target.name);
+    
+        updateFormState(()=>({...formState, [e.target.name]: e.target.value}))
+    }
+
+  return (
+  <>
+ <Header/>
+ <div>
    <section className="w-full bg-white">
         <div className="mx-auto max-w-7xl">
           <div className="flex flex-col lg:flex-row">
-            <div className="relative w-full bg-cover lg:w-6/12 xl:w-7/12 bg-gradient-to-r from-white via-white to-gray-100">
+               <div className="relative w-full bg-cover lg:w-6/12 xl:w-7/12 bg-gradient-to-r from-white via-white to-gray-100">
+              <div className="flex flex-col items-start justify-start w-full h-full p-10 lg:p-16 xl:p-24">
+                <div className="relative">
+                      <h4 className="text-lg font-extrabold mr-10 pb-4 tracking-tight text-center text-gray-900 sm:text-3xl md:text-xl lg:text-2xl xl:text-3xl">Get a Free Demo of Infohorus Ransomware Risk Management platform.</h4>
+    <p className=" mx-8 text-base pb-4 text-gray-800 sm:max-w-md lg:text-lg md:max-w-2xl">See for yourself how Infohorus can help you improve, assess and improve your cybersecurity posture, build a collaborative cybersecurity culture across your organization and prepare to respond and recover from any attacks you may encounter.</p>
+    <p className=" mx-8 text-base text-gray-800 sm:max-w-md lg:text-lg md:max-w-2xl">Infohorus Ransomware Risk Management platform is everything you need to be able to:</p>
+    <ul className="list-disc mx-12 text-base pb-4 text-gray-800 sm:max-w-md lg:text-lg md:max-w-2xl">
+     <li>Manage your enterprise-level and business process-level risks that expose your organization to ransomware attacks</li>
+     <li>Create a coordinated and collaborative cybersecurity program that includes every level of the organization.
+     </li>
+     <li>Benchmark your readiness in responding and dealing with a ransomware attack</li>
+     <li>Track the implementation of actions required to address existing gaps in your program at a strategic and operational level</li>
+     <li>Assign implementation actions to specific owners in different parts of your organization</li>
+     <li >Present operational and executive reports</li>
+     <li >… and a lot more all in one place</li>
+    </ul>
+    <p className=" mx-8 text-base pb-4 text-gray-800 sm:max-w-md lg:text-lg md:max-w-2xl">See for yourself by filling out the form to schedule a free demo customized to your specific organization.</p>
+                  
+                </div>
+              </div>
+            </div>
+            <div className="w-full bg-white lg:w-6/12 xl:w-5/12">
               <div className="relative flex flex-col items-center justify-center w-full h-full px-10 my-20 lg:px-16 lg:my-0">
                 <div className="flex flex-col items-start space-y-8 tracking-tight lg:max-w-3xl">
+                  <h4 className="w-full text-3xl font-bold">Request Demo</h4>
+                <div className="relative w-full mt-10 space-y-8">
+                  <div className="relative">
+                    <label className="font-medium text-gray-900">Name</label>
+                    <input type="text" onChange={onChange} name="fname" className="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Enter Your Name" />
+                  </div>
+                  
+                  <div className="relative">
+                    <label className="font-medium text-gray-900">Email</label>
+                    <input type="text" onChange={onChange} name="email" className="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Enter Your Email Address" />
+                  </div>
+                  <div className="relative">
+                    <label className="font-medium text-gray-900">Company</label>
+                    <input type="text" onChange={onChange} className="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Company" name="company" />
+                  </div>
+                  <div className="relative">
+                  <label for="employees" className="visually-hidden">Number of employees</label>  
+                  <Controller name="employees" className="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" control={control} as={Select} onChange={onChange} options={selectEmpOptions} defaultValue="Employees" />
+                   
+          </div>
+                  <div className="relative">
+                    <button type="submit" className="inline-block w-full px-5 py-4 text-lg font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease">Request Demo</button>
+                    
+                  </div>
+                </div>
+                
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    <Footer/>
+  </div>
+    </>
+ )
+}
+
+{/* <div className="flex flex-col items-start space-y-8 tracking-tight lg:max-w-3xl">
                   <div className="relative">
                       <h4 className="text-lg font-extrabold mr-20 pb-4 tracking-tight text-center text-gray-900 sm:text-3xl md:text-xl lg:text-2xl xl:text-3xl">Get a Free Demo of Infohorus Ransomware Risk Management platform.</h4>
     <p className=" mx-28 text-base pb-4 text-gray-800 sm:max-w-md lg:text-lg md:max-w-2xl">See for yourself how Infohorus can help you improve, assess and improve your cybersecurity posture, build a collaborative cybersecurity culture across your organization and prepare to respond and recover from any attacks you may encounter.</p>
@@ -30,16 +105,4 @@ export const Demo = () => {
     <p className=" mx-28 text-base pb-4 text-gray-800 sm:max-w-md lg:text-lg md:max-w-2xl">See for yourself by filling out the form to schedule a free demo customized to your specific organization.</p>
                   
                 </div>
-              </div>
-            </div>
-            <div className="w-full bg-white lg:w-6/12 xl:w-5/12">
-            {/* <Form formHead="Schedule Demo" formBtnText="Submit" /> */}
-            </div>
-          </div>
-        </div>
-        </div>
-      </section>
-   <Footer/>
-  </div>
- )
-}
+              </div> */}
