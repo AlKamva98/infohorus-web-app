8e31f37b3bfd8d3a77a3cf9a9b3cb835c96b2540
AlKamva98 <ngantwenistefano@gamail.co.za>
2021-06-22 15:37:38 +0200

    front end fix

--- src/pages/EndUser/Demo.js
+++ src/pages/EndUser/Demo.js
@@ -1,7 +1,7 @@
 import {Header} from '../../components/index/Header';
 import Select  from 'react-select';
 import {useForm, Controller } from "react-hook-form";
-import { selectEmpOptions} from '../../testData/selectOptions'
+import {selectOptionsCountry} from '../../testData/selectOptions'
 import Footer from '../../components/index/Footer';
 import React,{useState} from 'react'
 
@@ -49,33 +49,26 @@ export const Demo = () => {
             <div className="w-full bg-white lg:w-6/12 xl:w-5/12">
               <div className="relative flex flex-col items-center justify-center w-full h-full px-10 my-20 lg:px-16 lg:my-0">
                 <div className="flex flex-col items-start space-y-8 tracking-tight lg:max-w-3xl">
-                  <h4 className="w-full text-3xl font-bold">Request Demo</h4>
-                <div className="relative w-full mt-10 space-y-8">
-                  <div className="relative">
-                    <label className="font-medium text-gray-900">Name</label>
-                    <input type="text" onChange={onChange} name="fname" className="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Enter Your Name" />
-                  </div>
-                  
-                  <div className="relative">
-                    <label className="font-medium text-gray-900">Email</label>
-                    <input type="text" onChange={onChange} name="email" className="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Enter Your Email Address" />
-                  </div>
-                  <div className="relative">
-                    <label className="font-medium text-gray-900">Company</label>
-                    <input type="text" onChange={onChange} className="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Company" name="company" />
-                  </div>
-                  <div className="relative">
-                  <label for="employees" className="visually-hidden">Number of employees</label>  
-                  <Controller name="employees" className="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" control={control} as={Select} onChange={onChange} options={selectEmpOptions} defaultValue="Employees" />
-                   
-          </div>
-                  <div className="relative">
-                    <button type="submit" className="inline-block w-full px-5 py-4 text-lg font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease">Request Demo</button>
-                    
-                  </div>
-                </div>
-                
-                </div>
+  <h4 className="w-full text-3xl font-bold">Request Demo</h4>
+  <h5 className="w-full text-base ">Complete the form below and a representative will be in touch shortly to discuss your trial.</h5>
+  <form className="w-full mt-10 pb-3 space-y-8"  method="POST">
+    
+    <input type="text" onChange={onChange} name="fname" className=" w-full inline-block px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Name" required />
+        
+    <input type="text" onChange={onChange} className="w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" name="organisation" placeholder="Company" required />
+    <input type="text" onChange={onChange} className=" w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" name="jobtitle" placeholder="Job title" required />
+    <input type="text" onChange={onChange} name="email" className="w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Email Address" required />
+    <input type="tel" className="w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" name="phone" placeholder="Phone" required />
+    <Controller name="country" className="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" control={control} as={Select} onChange={onChange} options={selectOptionsCountry} defaultValue="Country" />
+    <p style={{color: 'grey'}}><span><input type="checkbox" required /></span> I agree to the <a href="bahati-tech-complete-online-privacy-statement (1).pdf" target="blank">Subscription Agreement.</a></p>
+    <p className=" text-base pb-4 text-gray-800 sm:max-w-md lg:text-lg md:max-w-2xl">By registering, you agree to the processing of your personal data by <a href="http://bahatitech.co.za" target="_blank">Bahati Tech</a> as described in the <a href="bahati-tech-complete-online-privacy-statement (1).pdf" target="blank">Privacy Statement.</a></p>
+    <p className=" text-base pb-4 text-gray-800 sm:max-w-md lg:text-lg md:max-w-2xl"><span><input type="checkbox" name="marketing" /></span> Yes, I would like to receive marketing communications regarding Bahati Tech products, services, and events. I can unsubscribe at a later time.
+    </p>
+     <div className="relative">
+            <button type="submit" className="inline-block w-full px-5 py-4 text-lg font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease">Request Demo</button>
+                                   </div>
+  </form></div>
+
               </div>
             </div>
           </div>
@@ -87,22 +80,33 @@ export const Demo = () => {
  )
 }
 
-{/* <div className="flex flex-col items-start space-y-8 tracking-tight lg:max-w-3xl">
-                  <div className="relative">
-                      <h4 className="text-lg font-extrabold mr-20 pb-4 tracking-tight text-center text-gray-900 sm:text-3xl md:text-xl lg:text-2xl xl:text-3xl">Get a Free Demo of Infohorus Ransomware Risk Management platform.</h4>
-    <p className=" mx-28 text-base pb-4 text-gray-800 sm:max-w-md lg:text-lg md:max-w-2xl">See for yourself how Infohorus can help you improve, assess and improve your cybersecurity posture, build a collaborative cybersecurity culture across your organization and prepare to respond and recover from any attacks you may encounter.</p>
-    <p className=" mx-28 text-base text-gray-800 sm:max-w-md lg:text-lg md:max-w-2xl">Infohorus Ransomware Risk Management platform is everything you need to be able to:</p>
-    <ul className="list-disc mx-32 text-base pb-4 text-gray-800 sm:max-w-md lg:text-lg md:max-w-2xl">
-     <li>Manage your enterprise-level and business process-level risks that expose your organization to ransomware attacks</li>
-     <li>Create a coordinated and collaborative cybersecurity program that includes every level of the organization.
-     </li>
-     <li>Benchmark your readiness in responding and dealing with a ransomware attack</li>
-     <li>Track the implementation of actions required to address existing gaps in your program at a strategic and operational level</li>
-     <li>Assign implementation actions to specific owners in different parts of your organization</li>
-     <li >Present operational and executive reports</li>
-     <li >… and a lot more all in one place</li>
-    </ul>
-    <p className=" mx-28 text-base pb-4 text-gray-800 sm:max-w-md lg:text-lg md:max-w-2xl">See for yourself by filling out the form to schedule a free demo customized to your specific organization.</p>
+// <div className="flex flex-col items-start space-y-8 tracking-tight lg:max-w-3xl">
+//                   <h4 className="w-full text-3xl font-bold">Request Demo</h4>
+//                 <div className="relative w-full mt-10 space-y-8">
+//                   <div className="relative">
+//                     <label className="font-medium text-gray-900">Name</label>
+//                     <input type="text" onChange={onChange} name="fname" className="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Enter Your Name" />
+//                   </div>
                   
-                </div>
-              </div> */}
+//                   <div className="relative">
+//                     <label className="font-medium text-gray-900">Email</label>
+//                     <input type="text" onChange={onChange} name="email" className="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Enter Your Email Address" />
+//                   </div>
+//                   <div className="relative">
+//                     <label className="font-medium text-gray-900">Company</label>
+//                     <input type="text" onChange={onChange} className="block w-full px-4 py-4 mt-2 text-xl placeholder-gray-400 bg-gray-200 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-600 focus:ring-opacity-50" placeholder="Company" name="company" />
+//                   </div>
+//                   <div className="relative">
+//                   <label for="employees" className="visually-hidden">Number of employees</label>  
+//                   
+                   
+//           </div>
+//                   <div className="relative">
+//                     <button type="submit" className="inline-block w-full px-5 py-4 text-lg font-medium text-center text-white transition duration-200 bg-blue-600 rounded-lg hover:bg-blue-700 ease">Request Demo</button>
+                    
+//                   </div>
+//                 </div>
+                
+//                 </div>
+
+ 
--- src/testData/selectOptions.js
+++ src/testData/selectOptions.js
@@ -40,3 +40,249 @@ export const selectOptionsIndustry = [
      { value:"Agriculture", label: "Agriculture"},
      { value:"Government", label: "Government"}
       ];
+// value: "Afganistan">Afghanistan
+//       value: "Albania">Albania
+//       value: "Algeria">Algeria
+//       value: "American Samoa">American Samoa
+//       value: "Andorra">Andorra
+//       value: "Angola">Angola
+//       value: "Anguilla">Anguilla
+//       value: "Antigua & Barbuda">Antigua &amp; Barbuda
+//       value: "Argentina">Argentina
+//       value: "Armenia">Armenia
+//       value: "Aruba">Aruba
+//       value: "Australia">Australia
+//       value: "Austria">Austria
+//       value: "Azerbaijan">Azerbaijan
+//       value: "Bahamas">Bahamas
+//       value: "Bahrain">Bahrain
+//       value: "Bangladesh">Bangladesh
+//       value: "Barbados">Barbados
+//       value: "Belarus">Belarus
+//       value: "Belgium">Belgium
+//       value: "Belize">Belize
+//       value: "Benin">Benin
+//       value: "Bermuda">Bermuda
+//       value: "Bhutan">Bhutan
+//       value: "Bolivia">Bolivia
+//       value: "Bonaire">Bonaire
+//       value: "Bosnia & Herzegovina">Bosnia &amp; Herzegovina
+//       value: "Botswana">Botswana
+//       value: "Brazil">Brazil
+//       value: "British Indian Ocean Ter">British Indian Ocean Ter
+//       value: "Brunei">Brunei
+//       value: "Bulgaria">Bulgaria
+//       value: "Burkina Faso">Burkina Faso
+//       value: "Burundi">Burundi
+//       value: "Cambodia">Cambodia
+//       value: "Cameroon">Cameroon
+//       value: "Canada">Canada
+//       value: "Canary Islands">Canary Islands
+//       value: "Cape Verde">Cape Verde
+//       value: "Cayman Islands">Cayman Islands
+//       value: "Central African Republic">Central African Republic
+//       value: "Chad">Chad
+//       value: "Channel Islands">Channel Islands
+//       value: "Chile">Chile
+//       value: "China">China
+//       value: "Christmas Island">Christmas Island
+//       value: "Cocos Island">Cocos Island
+//       value: "Colombia">Colombia
+//       value: "Comoros">Comoros
+//       value: "Congo">Congo
+//       value: "Cook Islands">Cook Islands
+//       value: "Costa Rica">Costa Rica
+//       value: "Cote DIvoire">Cote DIvoire
+//       value: "Croatia">Croatia
+//       value: "Cuba">Cuba
+//       value: "Curaco">Curacao
+//       value: "Cyprus">Cyprus
+//       value: "Czech Republic">Czech Republic
+//       value: "Denmark">Denmark
+//       value: "Djibouti">Djibouti
+//       value: "Dominica">Dominica
+//       value: "Dominican Republic">Dominican Republic
+//       value: "East Timor">East Timor
+//       value: "Ecuador">Ecuador
+//       value: "Egypt">Egypt
+//       value: "El Salvador">El Salvador
+//       value: "Equatorial Guinea">Equatorial Guinea
+//       value: "Eritrea">Eritrea
+//       value: "Estonia">Estonia
+//       value: "Ethiopia">Ethiopia
+//       value: "Falkland Islands">Falkland Islands
+//       value: "Faroe Islands">Faroe Islands
+//       value: "Fiji">Fiji
+//       value: "Finland">Finland
+//       value: "France">France
+//       value: "French Guiana">French Guiana
+//       value: "French Polynesia">French Polynesia
+//       value: "French Southern Ter">French Southern Ter
+//       value: "Gabon">Gabon
+//       value: "Gambia">Gambia
+//       value: "Georgia">Georgia
+//       value: "Germany">Germany
+//       value: "Ghana">Ghana
+//       value: "Gibraltar">Gibraltar
+//       value: "Great Britain">Great Britain
+//       value: "Greece">Greece
+//       value: "Greenland">Greenland
+//       value: "Grenada">Grenada
+//       value: "Guadeloupe">Guadeloupe
+//       value: "Guam">Guam
+//       value: "Guatemala">Guatemala
+//       value: "Guinea">Guinea
+//       value: "Guyana">Guyana
+//       value: "Haiti">Haiti
+//       value: "Hawaii">Hawaii
+//       value: "Honduras">Honduras
+//       value: "Hong Kong">Hong Kong
+//       value: "Hungary">Hungary
+//       value: "Iceland">Iceland
+//       value: "Indonesia">Indonesia
+//       value: "India">India
+//       value: "Iran">Iran
+//       value: "Iraq">Iraq
+//       value: "Ireland">Ireland
+//       value: "Isle of Man">Isle of Man
+//       value: "Israel">Israel
+//       value: "Italy">Italy
+//       value: "Jamaica">Jamaica
+//       value: "Japan">Japan
+//       value: "Jordan">Jordan
+//       value: "Kazakhstan">Kazakhstan
+//       value: "Kenya">Kenya
+//       value: "Kiribati">Kiribati
+//       value: "Korea North">Korea North
+//       value: "Korea Sout">Korea South
+//       value: "Kuwait">Kuwait
+//       value: "Kyrgyzstan">Kyrgyzstan
+//       value: "Laos">Laos
+//       value: "Latvia">Latvia
+//       value: "Lebanon">Lebanon
+//       value: "Lesotho">Lesotho
+//       value: "Liberia">Liberia
+//       value: "Libya">Libya
+//       value: "Liechtenstein">Liechtenstein
+//       value: "Lithuania">Lithuania
+//       value: "Luxembourg">Luxembourg
+//       value: "Macau">Macau
+//       value: "Macedonia">Macedonia
+//       value: "Madagascar">Madagascar
+//       value: "Malaysia">Malaysia
+//       value: "Malawi">Malawi
+//       value: "Maldives">Maldives
+//       value: "Mali">Mali
+//       value: "Malta">Malta
+//       value: "Marshall Islands">Marshall Islands
+//       value: "Martinique">Martinique
+//       value: "Mauritania">Mauritania
+//       value: "Mauritius">Mauritius
+//       value: "Mayotte">Mayotte
+//       value: "Mexico">Mexico
+//       value: "Midway Islands">Midway Islands
+//       value: "Moldova">Moldova
+//       value: "Monaco">Monaco
+//       value: "Mongolia">Mongolia
+//       value: "Montserrat">Montserrat
+//       value: "Morocco">Morocco
+//       value: "Mozambique">Mozambique
+//       value: "Myanmar">Myanmar
+//       value: "Nambia">Nambia
+//       value: "Nauru">Nauru
+//       value: "Nepal">Nepal
+//       value: "Netherland Antilles">Netherland Antilles
+//       value: "Netherlands">Netherlands (Holland, Europe)
+//       value: "Nevis">Nevis
+//       value: "New Caledonia">New Caledonia
+//       value: "New Zealand">New Zealand
+//       value: "Nicaragua">Nicaragua
+//       value: "Niger">Niger
+//       value: "Nigeria">Nigeria
+//       value: "Niue">Niue
+//       value: "Norfolk Island">Norfolk Island
+//       value: "Norway">Norway
+//       value: "Oman">Oman
+//       value: "Pakistan">Pakistan
+//       value: "Palau Island">Palau Island
+//       value: "Palestine">Palestine
+//       value: "Panama">Panama
+//       value: "Papua New Guinea">Papua New Guinea
+//       value: "Paraguay">Paraguay
+//       value: "Peru">Peru
+//       value: "Phillipines">Philippines
+//       value: "Pitcairn Island">Pitcairn Island
+//       value: "Poland">Poland
+//       value: "Portugal">Portugal
+//       value: "Puerto Rico">Puerto Rico
+//       value: "Qatar">Qatar
+//       value: "Republic of Montenegro">Republic of Montenegro
+//       value: "Republic of Serbia">Republic of Serbia
+//       value: "Reunion">Reunion
+//       value: "Romania">Romania
+//       value: "Russia">Russia
+//       value: "Rwanda">Rwanda
+//       value: "St Barthelemy">St Barthelemy
+//       value: "St Eustatius">St Eustatius
+//       value: "St Helena">St Helena
+//       value: "St Kitts-Nevis">St Kitts-Nevis
+//       value: "St Lucia">St Lucia
+//       value: "St Maarten">St Maarten
+//       value: "St Pierre & Miquelon">St Pierre &amp; Miquelon
+//       value: "St Vincent & Grenadines">St Vincent &amp; Grenadines
+//       value: "Saipan">Saipan
+//       value: "Samoa">Samoa
+//       value: "Samoa American">Samoa American
+//       value: "San Marino">San Marino
+//       value: "Sao Tome & Principe">Sao Tome &amp; Principe
+//       value: "Saudi Arabia">Saudi Arabia
+//       value: "Senegal">Senegal
+//       value: "Seychelles">Seychelles
+//       value: "Sierra Leone">Sierra Leone
+//       value: "Singapore">Singapore
+//       value: "Slovakia">Slovakia
+//       value: "Slovenia">Slovenia
+//       value: "Solomon Islands">Solomon Islands
+//       value: "Somalia">Somalia
+//       value: "South Africa">South Africa
+//       value: "Spain">Spain
+//       value: "Sri Lanka">Sri Lanka
+//       value: "Sudan">Sudan
+//       value: "Suriname">Suriname
+//       value: "Swaziland">Swaziland
+//       value: "Sweden">Sweden
+//       value: "Switzerland">Switzerland
+//       value: "Syria">Syria
+//       value: "Tahiti">Tahiti
+//       value: "Taiwan">Taiwan
+//       value: "Tajikistan">Tajikistan
+//       value: "Tanzania">Tanzania
+//       value: "Thailand">Thailand
+//       value: "Togo">Togo
+//       value: "Tokelau">Tokelau
+//       value: "Tonga">Tonga
+//       value: "Trinidad & Tobago">Trinidad &amp; Tobago
+//       value: "Tunisia">Tunisia
+//       value: "Turkey">Turkey
+//       value: "Turkmenistan">Turkmenistan
+//       value: "Turks & Caicos Is">Turks &amp; Caicos Is
+//       value: "Tuvalu">Tuvalu
+//       value: "Uganda">Uganda
+//       value: "United Kingdom">United Kingdom
+//       value: "Ukraine">Ukraine
+//       value: "United Arab Erimates">United Arab Emirates
+//       value: "United States of America">United States of America
+//       value: "Uraguay">Uruguay
+//       value: "Uzbekistan">Uzbekistan
+//       value: "Vanuatu">Vanuatu
+//       value: "Vatican City State">Vatican City State
+//       value: "Venezuela">Venezuela
+//       value: "Vietnam">Vietnam
+//       value: "Virgin Islands (Brit)">Virgin Islands (Brit)
+//       value: "Virgin Islands (USA)">Virgin Islands (USA)
+//       value: "Wake Island">Wake Island
+//       value: "Wallis & Futana Is">Wallis &amp; Futana Is
+//       value: "Yemen">Yemen
+//       value: "Zaire">Zaire
+//       value: "Zambia">Zambia
+//       value: "Zimbabwe">Zimbabwe
