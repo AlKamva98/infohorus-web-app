14c2016c8c90882644aa12daac4d6142ca72503d
AlKamva98 <ngantwenistefano@gamail.co.za>
2021-07-05 08:39:56 +0200

    Show dashboard

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -20,11 +20,11 @@ export const MenuItems =[
   url: '/contact',
   cName : 'relative font-medium leading-6 text-gray-600 transition duration-150 ease-out hover:text-gray-900'
  },
- // {
- //  title: 'Dashboard',
- //  url: '/findings ',
- //  cName: 'relative font-medium leading-6 text-gray-600 transition duration-150 ease-out hover:text-gray-900'
- // },
+ {
+  title: 'Dashboard',
+  url: '/dasboard ',
+  cName: 'relative font-medium leading-6 text-gray-600 transition duration-150 ease-out hover:text-gray-900'
+ },
   {
   title: 'Sign in',
   url: '#',
