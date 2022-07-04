d81cc90d56f0cbff0d1613c8ec83b13a1a151cf1
AlKamva98 <ngantwenistefano@gmail.com>
2022-07-04 11:15:04 +0200

    autofocus fix

--- src/Home/views/index/ContactSec.js
+++ src/Home/views/index/ContactSec.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function ContactSec(props) {
     return (
@@ -8,7 +9,7 @@ function ContactSec(props) {
                 <h1 className="text-4xl py-4 text-white text-center font-bold tracking-tight ">Contact Us</h1>
                 <p className='text-base text-gray-100 text-center mx-32'>Do you have questions about which products or services are right for your organization? We would be delighted to assist you.</p>
                 <p className='text-base text-gray-100 text-center'>Please fill out the form and a Bahati Tech Representative will follow up with you by phone or e-mail.</p>
-            <button className='flex items-center w-full btn px-6 py-3 mb-3 text-lg text-white bg-gray-900 rounded-full sm:mb-0 hover:bg-gray-700 sm:w-auto'>Send Message</button>
+            <Link to="/main/contact" className='flex items-center w-full btn px-6 py-3 mb-3 text-lg text-white bg-gray-900 rounded-full sm:mb-0 hover:bg-gray-700 sm:w-auto'>Send Message</Link>
             </div>
         </div>
     );
